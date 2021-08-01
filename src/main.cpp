0370523300637f33ce45cb2e9c6a82e5b37f4458
Jakub Piasecki <jakpia21@gmail.com>
2021-08-01 20:11:20 +0200

    incorporated glog into progen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <glog/logging.h>
 
-int main() {
-    std::cout << "Hello world!\n";
+int main(int argc, char* argv[]) {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_logtostderr = 1;
+    VLOG(1) << "Starting ProGen";
 
+
+    VLOG(1) << "ProGen exited succesfully";
     return 0;
 }

bf42cc07599cdd92092b832fdf353241657fdd9a
Maciej <wrobcio789@gmail.com>
2021-08-02 21:17:11 +0200

    Create basic program architecture

--- src/pcg/SimpleLevelGenerator.cpp
+++ src/pcg/SimpleLevelGenerator.cpp
@@ -9,17 +9,17 @@ void SimpleLevelGenerator::run(Scene &scene) {
     float room_height = bounding_box.get_height() / 4;
     float corridor_length = bounding_box.get_width() / 3;
 
-    BoundingBox leftRoomBox = BoundingBox(
+    BoundingBox left_room_box = BoundingBox(
             bounding_box.get_middle().shifted_by(-corridor_length / 2 - room_width, -room_height / 2),
             bounding_box.get_middle().shifted_by(-corridor_length / 2, room_height / 2));
-    SimpleRoomGenerator leftRoomGenerator(leftRoomBox);
-    leftRoomGenerator.run(scene);
+    SimpleRoomGenerator left_room_generator(left_room_box);
+    left_room_generator.run(scene);
 
-    BoundingBox rightRoomBox = BoundingBox(
+    BoundingBox right_room_box = BoundingBox(
             bounding_box.get_middle().shifted_by(corridor_length / 2, -room_height / 2),
             bounding_box.get_middle().shifted_by(corridor_length / 2 + room_width, room_height / 2));
-    SimpleRoomGenerator rightRoomGenerator(rightRoomBox);
-    rightRoomGenerator.run(scene);
+    SimpleRoomGenerator right_room_generator(right_room_box);
+    right_room_generator.run(scene);
 
     //Skipping generating a corridor for now
     DLOG(INFO) << "SimpleLevelGenerator finished";
--- tests/common/point_test.cpp
+++ tests/common/point_test.cpp
@@ -4,7 +4,7 @@
 TEST(Point_test, shifting_test) {
     Point point(21, 37);
 
-    Point shiftedPoint = point.shifted_by(4, 20);
+    Point shifted_point = point.shifted_by(4, 20);
 
-    ASSERT_EQ(shiftedPoint, Point(25, 57));
+    ASSERT_EQ(shifted_point, Point(25, 57));
 }

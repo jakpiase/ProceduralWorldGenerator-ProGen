384ecbcfc801d169a49a034bcd69bb70b647f168
Jakub Piasecki <jakpia21@gmail.com>
2021-11-27 19:08:01 +0100

    added tf model

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,25 @@
 #include <iostream>
 #include <glog/logging.h>
 #include <SDL.h>
+//#include <cnpy.h>
 #include <exception>
 #include "src/game.h"
 
 #undef main //SDL defines main and it needs to be undefined
 
+void init_data() {
+    system("python.exe infer.py");
+
+    std::cout << "finished";
+}
 
 int main(int argc, const char* argv[]) {
     FLAGS_logtostderr = true;
 
     google::InitGoogleLogging(argv[0]);
 
+    init_data();
+    return 0;
     if (SDL_Init(SDL_INIT_VIDEO) < 0){
         LOG(ERROR) << "Could not initialize SDL! Error: " << SDL_GetError();
     }
---
+++ src/python/infer.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+
+tf.__version__
+
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+from board_operations import *
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(1*1*128, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Reshape((1, 1, 128)))
+    assert model.output_shape == (None, 1, 1, 128)
+
+    model.add(layers.Conv2DTranspose(128, (5, 5)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(128, (3, 3), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2DTranspose(64, (3, 3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(64, (3, 3), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(32, (3, 3), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(NUMBER_OF_TILE_TYPES, (3, 3), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Softmax())
+
+    assert model.output_shape == (None, 7, 7, NUMBER_OF_TILE_TYPES)
+
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(NUMBER_OF_TILE_TYPES, (3, 3), padding='same', input_shape=[7, 7, NUMBER_OF_TILE_TYPES]))
+    model.add(layers.LayerNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(32, (3, 3), padding='same'))
+    model.add(layers.LayerNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(32, (3, 3), padding='same'))
+    model.add(layers.LayerNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(64, (3, 3), padding='same'))
+    model.add(layers.LayerNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(64, (3, 3)))
+    model.add(layers.LayerNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(128, (3, 3), padding='same'))
+    model.add(layers.LayerNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+
+    model.add(layers.Conv2D(128, (5, 5)))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(1))
+
+    return model
+
+def load_model():
+    checkpoint_dir = './'
+
+    generator = make_generator_model()
+    discriminator = make_discriminator_model()
+
+    generator_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5, beta_1=0.5, beta_2=0.9)
+    discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-5, beta_1=0.5, beta_2=0.9)
+
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                    discriminator_optimizer=discriminator_optimizer,
+                                    generator=generator,
+                                    discriminator=discriminator)
+    checkpoint.restore("ckpt-83").expect_partial()
+    return generator
+
+def infer():
+    generator = load_model()
+    noise_dim = 100
+    num_examples_to_generate = 5000
+
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
+    predictions = generator(seed, training=False)
+    predictions = predictions.numpy()
+
+    first = True
+    result = []
+
+    for i in range(predictions.shape[0]):
+        room = np.expand_dims(predictions[i], 0)
+        room = transform_from_one_hot(room)
+
+        if(valid_room(room)):
+            if(first == True):
+                result = np.reshape(room, (1, 7, 7))
+                first = False
+            else:
+                result = np.concatenate((result, np.reshape(room, (1, 7, 7))))
+
+    #print(result.shape)
+    #print("saving", result.shape[0], "images")
+    np.save("generated_images", result)
+
+infer()

8f8556c2de4aa4efd8af3d340c981da171737c9e
jakpiase <jakpia21@gmail.com>
2021-11-28 02:00:45 +0100

    fixed mersene twister implementation

--- src/pcg/utils/random_iterators.h
+++ src/pcg/utils/random_iterators.h
@@ -31,8 +31,9 @@ public:
 
 
 private:
-    void shuffle() {
-        auto random_number_engine = std::default_random_engine(rng.random());
+    void shuffle() { auto random_number_engine =
+        std::mersenne_twister_engine<unsigned int, 32, 624, 397, 31, 0x9908b0df, 11, 0xffffffff, 7,
+                                     0x9d2c5680, 15,0xefc60000, 18, 1812433253>(rng.random());
         std::shuffle(elements.begin(), elements.end(), random_number_engine);
     }
 
